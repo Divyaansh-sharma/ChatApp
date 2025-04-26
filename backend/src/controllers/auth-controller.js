a2b9248762b3a5c634749dc97b8b8cbf730f6b2d
Divyansh <uic.16mca8177@gmail.com>
2025-04-26 18:45:21 +0530

    initial-project-setup

---
+++ backend/src/controllers/auth-controller.js
@@ -0,0 +1,17 @@
+
+
+export const signup=(req, res) => {
+    
+}
+
+
+
+export const login=(req, res) => {
+    res.send("signup route")
+}
+
+
+
+export const logout=(req, res) => {
+    res.send("signup route")
+}
